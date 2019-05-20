e9918680331b085d5b9b57ab42a067562f4adf3e
Miranda84315 <miranda84315@gmail.com>
2019-05-20 15:32:12 +0800

    fun label work->clipvideo

--- src/video_clip/clipVideo.py
+++ src/video_clip/clipVideo.py
@@ -1,16 +1,21 @@
 import cv2
 import os
-video_root = 'D:/Data/0318/'
+video_root = 'D:/Data/0415/'
 video_filename = [
-    'XVR_ch1_main_20190318110001_20190318114828.avi',
-    'XVR_ch2_main_20190318110001_20190318114828.avi',
-    'XVR_ch3_main_20190318110000_20190318114828.avi',
-    'XVR_ch4_main_20190318110000_20190318114828.avi']
-save_root = 'D:/Data/dataset/Player14/track'
-save_dir = 'D:/Data/dataset/Player14'
+    'XVR_ch1_main_20190415110000_20190415113822.avi',
+    'XVR_ch2_main_20190415110001_20190415113822.avi',
+    'XVR_ch3_main_20190415110000_20190415113822.avi',
+    'XVR_ch4_main_20190415110000_20190415113822.avi']
+video_filename2 = [
+    'XVR_ch1_main_20190415110000_20190415113822.avi',
+    'XVR_ch2_main_20190415110001_20190415113822.avi',
+    'XVR_ch3_main_20190415110000_20190415113822.avi',
+    'XVR_ch4_main_20190415110000_20190415113822.avi']
+save_root = 'D:/Data/dataset/Player31/track'
+save_dir = 'D:/Data/dataset/Player31'
 fps = 15
-frame_start = [23483, 24994, 26200, 27654, 42736, 43405, 43930, 44705]
-frame_end = [23733, 25284, 26393, 27929, 43036, 43641, 44261, 44888]
+frame_start = [36344, 37322, 38384, 39272, 3488, 3833, 4402, 4889]
+frame_end = [36508, 37549, 38581, 39403, 3694, 4032, 4766, 5226]
 
 if not os.path.exists(save_dir):
     os.mkdir(save_dir)
@@ -24,7 +29,7 @@ for track in range(0, 8):
 for icam in range(0, 4):
     filename = video_root + video_filename[icam]
     cap = cv2.VideoCapture(filename)
-    for clip_num in range(0, 8):
+    for clip_num in range(4, 8):
         fourcc = cv2.VideoWriter_fourcc(*'XVID')
         out_filename = save_root + str(clip_num + 1) + '/cam' + str(icam + 1) + '.avi'
         print('======================================')
@@ -32,6 +37,7 @@ for icam in range(0, 4):
         height = 1080
         width = 1920
         out = cv2.VideoWriter(out_filename, fourcc, fps, (width, height))
+        
         cap.set(1, frame_start[clip_num] - 1)
         for i in range(frame_start[clip_num] - 1, frame_end[clip_num]):
             # print(i)
@@ -39,6 +45,25 @@ for icam in range(0, 4):
             cv2.imshow("video", frame)
             cv2.waitKey(1)
             out.write(frame)
+        '''
+        # if end < start
+        cap.set(1, frame_start[clip_num] - 1)
+        for i in range(frame_start[clip_num] - 1, int(cap.get(cv2.CAP_PROP_FRAME_COUNT))):
+            # print(i)
+            ret, frame = cap.read()
+            cv2.imshow("video", frame)
+            cv2.waitKey(1)
+            out.write(frame)
+
+        filename = video_root + video_filename2[icam]
+        cap = cv2.VideoCapture(filename)
+
+        for i in range(0, frame_end[clip_num]):
+            ret, frame = cap.read()
+            cv2.imshow("video", frame)
+            cv2.waitKey(1)
+            out.write(frame)
+        '''
         out.release()
 
 cv2.destroyAllWindows()
